aef8bf148a4b9c418dd3779fec14693397e37923
gyk <17212010059@fudan.edu.cn>
2019-02-27 23:41:21 +0800

    javascript study

--- js/test.js
+++ js/test.js
@@ -1,5 +1,17 @@
-var a=10;
-for (let i=0;i<10;i++){
-    a=a+i;
-}
-console.log(a);
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortArrayByParity = function(A) {
+    for (let i=0;i<A.length;i++){
+        var B=[];
+        var C=[];
+        if (A[i]%2===0){
+            B.concat(A[i]);
+        }
+        else {
+            C.concat(A[i]);
+        }
+    }
+    return [...B, ...C];
+};
