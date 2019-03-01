b81bbaeeadb5786d1fdca3709e025ebf46067c03
gyk <17212010059@fudan.edu.cn>
2019-03-01 22:00:33 +0800

    homework 0228 so hard

--- js/test.js
+++ js/test.js
@@ -1,17 +1,7 @@
 /**
- * @param {number[]} A
- * @return {number[]}
+ * @param {number[][]} A
+ * @return {number[][]}
  */
-var sortArrayByParity = function(A) {
-    for (let i=0;i<A.length;i++){
-        var B=[];
-        var C=[];
-        if (A[i]%2===0){
-            B.concat(A[i]);
-        }
-        else {
-            C.concat(A[i]);
-        }
-    }
-    return [...B, ...C];
+var flipAndInvertImage = function(A) {
+
 };
